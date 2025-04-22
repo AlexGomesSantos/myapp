f2df17aae385090b62aacf57f7a5264e54241c07
Alex gomes santos <65431675+AlexGomesSantos@users.noreply.github.com>
2025-04-22 18:09:03 +0100

    adiconado os botões

---
+++ Desktop/Dev/my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import Image from "./img/people.svg";
+
+import "./App.css";
+
+function App() {
+  function cliqueiNoBotao() {
+    console.log("Clicou no botão");
+  }
+
+  return (
+    <div>
+      <img src={Image} alt="imagem-pessoas" />
+      <textarea></textarea>
+      <button onClick={cliqueiNoBotao}>Comentar</button>
+
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
